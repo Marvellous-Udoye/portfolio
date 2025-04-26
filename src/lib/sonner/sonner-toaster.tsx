20a555310216e6452d333dfb5e5033bac8b3452b
Marv_lous <155150860+Marvellous-Udoye@users.noreply.github.com>
2025-04-27 00:20:30 +0100

    chore: remove unused dependencies and directories

--- src/lib/sonner/sonner-toaster.tsx
+++
@@ -1,7 +0,0 @@
-"use client";
-import React from "react";
-import { Toaster } from "sonner";
-
-export const SonnerToaster = () => {
-  return <Toaster position="top-right" richColors expand={false} />;
-};
--- src/schemas/login-schema.ts
+++
@@ -1,9 +0,0 @@
-import { z } from "zod";
-
-export const ImageAdSchema = z.object({
-  username: z
-    .string()
-    .min(8, "Username must be at least 8 characters")
-    .max(20, "Username cannot exceed 20 characters")
-    .nonempty("Username is required"),
-});
